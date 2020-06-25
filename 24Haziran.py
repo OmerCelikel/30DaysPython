56548b11a3d9176cfca04f3dfa8b2d756c569521
OmerCelikel <44753206+OmerCelikel@users.noreply.github.com>
2020-06-25 09:55:31 +0300

    Add files via upload

---
+++ 24Haziran.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+string ="gollum"
+
+#i stringde dolaş
+for i in string:
+    print(i)
+
+
+liste = ["ömer"]
+
+for i in liste:
+    print (i)
+
+# 10 lık oluşturduk , 2 den 10 a kadar
+for i in range(2,10):
+    print(i , ". değer artıyor")
+
+# faktöriyel
+a = 1
+for i in range(1,6):
+    a *= i
+    print(i ,". faktöriyel : " , a)
+
+
+
