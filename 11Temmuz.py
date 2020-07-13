dcf2e5affa85d8438e788f5809fa83405790ded2
OmerCelikel <44753206+OmerCelikel@users.noreply.github.com>
2020-07-13 07:47:50 +0300

    Add files via upload

---
+++ 11Temmuz.py
@@ -0,0 +1,6 @@
+deneme = 'merhaba\nNaber'
+print(deneme)
+
+deneme2 = 'X\nY'
+print(deneme2)
+print(len(deneme2)) #3 çünkü \n 1 sayılıyor
---
+++ 4Temmuz.py
@@ -0,0 +1,32 @@
+str1 = "Hello"
+str2 = 'there'
+bob = str1 + str2
+print(bob)
+print (str1,str2)
+
+# imposibble printt(str1+str2)
+
+x = '40'
+y = int(x) + 2
+print(y)
+
+x1 = 'From marquard@uct.ac.za'
+print(x1[8])
+print(x1[14 :17 ])
+
+for letter in 'banana' :
+    print(letter)
+
+# ---------------------------------------------
+
+# printing 0.8475 and converting float
+text = "X-DSPAM-Confidence:    0.8475";
+
+spacem = text.find("0")
+print (spacem)
+
+print(text[spacem:])
+anumber = float(text[spacem:])
+
+print(anumber)
+# ---------------------------------------------
